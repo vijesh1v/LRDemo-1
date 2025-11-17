78fa28452499f46d46290e9f97bcbbe9ae9300c9
Vijesh <vijeshv.in@gmail.com>
2025-11-17 21:56:08 +0530

    upload lr

---
+++ Action.c
@@ -0,0 +1,15 @@
+Action()
+{
+
+	web_custom_request("web_custom_request1",
+		"URL=https://www.google.com",
+		"Method=GET",
+		"TargetFrame=",
+		"Resource=0",
+		"Referer=",
+		"Body=",
+		LAST);
+
+	
+	return 0;
+}
